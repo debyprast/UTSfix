88320e1e3199b29eb2aa2434ac1c757a5c16516f
debyprast <debyprast18@gmail.com>
2019-10-21 00:43:33 +0700

    Logika untuk passing data antar fragment

---
+++ app/src/main/java/org/jplas/android/utsfix/activty/ShowRoomActivity.java
@@ -0,0 +1,80 @@
+package org.jplas.android.utsfix.activty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ShowRoomActivity implements Parcelable {
+
+    private String nama;
+    private String motor;
+    private int jumlah;
+    private int hp;
+    private String alamat;
+
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public ShowRoomActivity(String nama, String motor, int jumlah, int hp, String alamat){
+        this.nama = nama;
+        this.motor = motor;
+        this.jumlah = jumlah;
+        this.hp = hp;
+        this.alamat = alamat;
+    }
+
+    protected ShowRoomActivity(Parcel in) {
+        nama = in.readString();
+        motor = in.readString();
+        jumlah = in.readInt();
+        hp = in.readInt();
+        alamat = in.readString();
+    }
+
+    public static final Parcelable.Creator<ShowRoomActivity> CREATOR = new Parcelable.Creator<ShowRoomActivity>() {
+
+        @Override
+        public ShowRoomActivity createFromParcel(Parcel in) {
+            return new ShowRoomActivity(in);
+        }
+
+        @Override
+        public ShowRoomActivity[] newArray(int size) {
+            return new ShowRoomActivity[size];
+        }
+    };
+
+    public int getJumlah(){
+        return jumlah;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(nama);
+        parcel.writeString(motor);
+        parcel.writeInt(jumlah);
+        parcel.writeInt(hp);
+        parcel.writeString(alamat);
+    }
+}
--- app/src/main/java/org/jplas/android/utsfix/fragment/ShowRoomFragment.java
+++ app/src/main/java/org/jplas/android/utsfix/fragment/ShowRoomFragment.java
@@ -4,17 +4,31 @@ package org.jplas.android.utsfix.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import org.jplas.android.utsfix.R;
+import org.jplas.android.utsfix.activty.ShowRoomActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ShowRoomFragment extends Fragment {
+public class ShowRoomFragment extends Fragment implements View.OnClickListener {
+
+
+
+    View view;
+    Button submit;
+    RadioGroup radio;
+    EditText namaText, motorText, jumlahText, hpText, alamatText;
 
 
     public ShowRoomFragment() {
@@ -26,7 +40,39 @@ public class ShowRoomFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_room, container, false);
+        view = inflater.inflate(R.layout.fragment_show_room, container, false);
+
+        radio = view.findViewById(R.id.radio);
+        namaText = view.findViewById(R.id.textNama);
+        motorText = view.findViewById(R.id.textMotor);
+        jumlahText = view.findViewById(R.id.textJumlah);
+        hpText = view.findViewById(R.id.textHp);
+        alamatText = view.findViewById(R.id.textAlamat);
+        submit = (Button) view.findViewById(R.id.button);
+        submit.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        String nama = namaText.getText().toString();
+        String motor = motorText.getText().toString();
+        int jumlah = Integer.parseInt(jumlahText.getText().toString());
+        int hp = Integer.parseInt(hpText.getText().toString());
+        String alamat = alamatText.getText().toString();
+
+            ShowRoomActivity kenal = new ShowRoomActivity(nama, motor, jumlah,hp,alamat);
+            Bundle args = new Bundle();
+            args.putParcelable("kenal", kenal);
+            Fragment Kalimat = new ShowRoomFragment();
+            Kalimat.setArguments(args);
+            FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragmenContainer, Kalimat);
+            fragmentTransaction.commit();
+        }else {
+            Toast.makeText(getActivity(), "Please select equipment and your name !", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
